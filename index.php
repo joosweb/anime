2bd57d4edf10ea5c85b7636029f2157f2fe1682a
Jose Osses <Koxe@MacBook-Pro-de-Jose.local>
2016-04-17 16:32:14 -0300

    AnimePhones
    Pagina web (PHP- MYSQL) de videos y series animadas.

---
+++ index.php
@@ -0,0 +1,166 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <title>AnimePhones | Ver Anime Online en HD</title>
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.min.js"></script>
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="header-top">
+        <div class="logo">
+          <img src="img/logo.png" alt="">
+        </div>
+      </div>
+      <!-- INICIO DEL NAV-->
+      <div class="well">
+      <nav class="navbar navbar-inverse">
+      <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#"></a>
+        </div>
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul class="nav navbar-nav">
+            <li class="active"><a href="#">INICIO<span class="sr-only">(current)</span></a></li>
+            <li><a href="#">ANIME ONLINE</a></li>
+          </ul>
+        </div><!-- /.navbar-collapse -->
+      </div><!-- /.container-fluid -->
+    </nav> <!-- FIN DEL NAV-->
+    <!--  INICIO BARRA LAST-->
+    <!--  FIN BARRA LAST-->
+    
+    <!--- SLIDER-->
+    <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
+  <!-- Indicators -->
+
+  <ol class="carousel-indicators">
+    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
+    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
+  </ol>
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="img/slide.jpg" alt="..." width="100%">
+      <div class="carousel-caption">
+      </div>
+    </div>
+    <div class="item">
+      <img src="img/slide.jpg" alt="..." width="100%">
+      <div class="carousel-caption">
+      </div>
+    </div>
+
+  </div>
+  <!-- Controls -->
+  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+
+    <!--- FIN SLIDER -->
+    <div class="row" style="margin-top:5px;">
+    <div class="col-md-8">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title">ULTIMAS PUBLICACIONES</h3>
+      </div>
+      <div class="panel-body">
+      <!--- -->
+      <div class="col-md-4">
+         <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">Panel primary</h3>
+          </div>
+          <div class="panel-body">
+            Panel content
+          </div>
+       </div>       
+      </div>
+      <!--- -->
+       <!--- -->
+      <div class="col-md-4">
+         <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">Panel primary</h3>
+          </div>
+          <div class="panel-body">
+            Panel content
+          </div>
+       </div>       
+      </div>
+      <!--- -->
+       <!--- -->
+      <div class="col-md-4">
+         <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">Panel primary</h3>
+          </div>
+          <div class="panel-body">
+            Panel content
+          </div>
+       </div>       
+      </div>
+      <!--- -->
+       <!--- -->
+      <div class="col-md-4">
+         <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">Panel primary</h3>
+          </div>
+          <div class="panel-body">
+            Panel content
+          </div>
+       </div>       
+      </div>
+      <!--- --> <!--- -->
+      <div class="col-md-4">
+         <div class="panel panel-default">
+          <div class="panel-heading">
+            <h3 class="panel-title">Panel primary</h3>
+          </div>
+          <div class="panel-body">
+            Panel content
+          </div>
+       </div>       
+      </div>
+      <!--- -->
+      </div>
+    </div>
+    </div>
+    <div class="col-md-4">
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h3 class="panel-title">FACEBOOK</h3>
+      </div>
+      <div class="panel-body">
+        Panel content
+      </div>
+    </div>
+    </div>
+    </div>
+    </div>
+
+    </div>
+  </div>
+  </div>
+</body>
+</html>
